7b32bcc63249197beab90be6bfca1260d5cda139
Nyaa <just.nyaa@protonmail.com>
2022-05-20 22:11:26 -0400

    Add inventory

--- packages/react-app/src/components/items/index.jsx
+++ packages/react-app/src/components/items/index.jsx
@@ -6,26 +6,35 @@ export default function Items() {
   const [itemLevels, setItemLevels] = useState([1, 1, 1]);
 
   const Inventory = () => {
-    const itemImages = itemLevels.map((itemLevel, index) => (
-      <div style={{ backgroundImage: `url(/items/${index + 1}.png)` }} className="item" key={`item-${index}`} />
-    ));
+    const itemImages = (
+      <div className="inventory-row">
+        <div className="inventory-info"></div>
+        <img
+          src={`/images/accessory_${itemLevels[0]}.webp`}
+          alt={`accessory level ${itemLevels[0]}`}
+          className="item"
+        />
+        <img src={`/images/armor_${itemLevels[1]}.webp`} alt={`armor level ${itemLevels[1]}`} className="item" />
+        <img src={`/images/weapon_${itemLevels[2]}.webp`} alt={`weapon level ${itemLevels[2]}`} className="item" />
+      </div>
+    );
 
     return <div className="inventory-container">{itemImages}</div>;
   };
 
-  // TODO: Change item images to be dynamic
+  // TODO: Connect item onClick to contract's item upgrade
   const Upgrades = () => {
     const upgradeImages = [];
-    for (let i = 1; i < 6; i++) {
+    for (let rowIdx = 1; rowIdx < 6; rowIdx++) {
       upgradeImages.push(
-        <div className="upgrade-row" key={`upgrade-${i}`}>
+        <div className="upgrade-row" key={`upgrade-${rowIdx}`}>
           <div className="upgrade-info">
-            <div className="upgrade-level">Lvl {i}</div>
-            {i !== 1 && <div className="upgrade-cost">{(i - 1) * 100}g</div>}
+            <div className="upgrade-level">Lvl {rowIdx}</div>
+            {rowIdx !== 1 && <div className="upgrade-cost">{(rowIdx - 1) ** 3 * 100}g</div>}
           </div>
-          <img src={`/images/accessory_${i}.webp`} alt={`accessory level ${i}`} className="item" />
-          <img src={`/images/armor_${i}.webp`} alt={`armor level ${i}`} className="item" />
-          <img src={`/images/weapon_${i}.webp`} alt={`weapon level ${i}`} className="item" />
+          <img src={`/images/accessory_${rowIdx}.webp`} alt={`accessory level ${rowIdx}`} className="item" />
+          <img src={`/images/armor_${rowIdx}.webp`} alt={`armor level ${rowIdx}`} className="item" />
+          <img src={`/images/weapon_${rowIdx}.webp`} alt={`weapon level ${rowIdx}`} className="item" />
         </div>
       );
     }
